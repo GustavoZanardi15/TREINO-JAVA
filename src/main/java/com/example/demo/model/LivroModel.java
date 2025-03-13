5cb1ce74d2868421f4bc57308558f3522183fcd6
ulianzanardig@gmail.com <ulianzanardig@gmail.com>
2025-03-13 10:10:39 -0300

    ADD: Classe LivroModel

--- src/main/java/com/example/demo/model/LivroModel.java
+++ src/main/java/com/example/demo/model/LivroModel.java
@@ -1,4 +1,63 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class LivroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
+    private String autor;
+    private String dataLancamento;
+
+    public LivroModel(){
+
+    }
+
+    public LivroModel(int id, String titulo, String autor, String dataLancamento){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataLancamento = dataLancamento;
+
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+
 }
