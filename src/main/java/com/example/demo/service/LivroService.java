7b1357ac3efd41f4e5f111cb24fa69bffcffab25
ulianzanardig@gmail.com <ulianzanardig@gmail.com>
2025-03-13 14:28:15 -0300

    ADD: Começo da criação do LivroService

--- src/main/java/com/example/demo/service/LivroService.java
+++ src/main/java/com/example/demo/service/LivroService.java
@@ -1,4 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class LivroService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+
 }
